3c57d44117e46be8c3f6fcb02a20e163cf2a9b5e
sora-blue <sora-blue@github.com>
2022-09-10 12:58:06 +0800

    lab-pdtbl: fix relying on freed page table

--- kernel/proc.c
+++ kernel/proc.c
@@ -521,6 +521,8 @@ scheduler(void)
         c->proc = p;
         swtch(&c->context, &p->context);
 
+        kvminithart();
+
         // Process is done running for now.
         // It should have changed its p->state before coming back.
         c->proc = 0;
