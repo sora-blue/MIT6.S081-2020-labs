9134306f86b01975a59201aad04ff124cbbfcc62
sora-blue <sora-blue@github.com>
2022-09-18 16:52:58 +0800

    lab-thread: pass first one

--- user/uthread.c
+++ user/uthread.c
@@ -12,6 +12,20 @@
 
 
 struct thread {
+  uint64     ra;
+  uint64     sp;
+  uint64     s0;
+  uint64     s1;
+  uint64     s2;
+  uint64     s3;
+  uint64     s4;
+  uint64     s5;
+  uint64     s6;
+  uint64     s7;
+  uint64     s8;
+  uint64     s9;
+  uint64     s10;
+  uint64     s11;
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
 
@@ -63,6 +77,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+     thread_switch((uint64)t, (uint64)next_thread);
   } else
     next_thread = 0;
 }
@@ -77,6 +92,9 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->ra = (uint64)func;
+  t->sp = (uint64)t->stack + STACK_SIZE;
+  t->s0 = t->sp;
 }
 
 void 
