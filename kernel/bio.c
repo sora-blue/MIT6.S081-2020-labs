f9b660f33639919daec6625a379939bf34254669
sora-blue <sora-blue@github.com>
2022-09-24 16:29:51 +0800

    lab-locks: pass bcachetest but randomly fail usertests

--- kernel/bio.c
+++ kernel/bio.c
@@ -23,32 +23,74 @@
 #include "fs.h"
 #include "buf.h"
 
+#define BUF_TABLE_SIZE 16
+
 struct {
   struct spinlock lock;
   struct buf buf[NBUF];
 
-  // Linked list of all buffers, through prev/next.
-  // Sorted by how recently the buffer was used.
-  // head.next is most recent, head.prev is least.
-  struct buf head;
+  struct buf *buftable[BUF_TABLE_SIZE];
+  struct spinlock btlock[BUF_TABLE_SIZE];
 } bcache;
+
+inline int
+bthash(int bufno)
+{
+  return bufno % BUF_TABLE_SIZE;
+}
+
+// caller acquire lock
+void
+btinsert(struct buf *b, int idx)
+{
+  struct buf *head;
+
+  head = bcache.buftable[idx];
+  if(head == b){
+    printf("Oops! Conflict!\n");
+    return;
+  }
+  bcache.buftable[idx] = b;
+  b->next = head;
+}
+
+// caller acquire lock
+void
+btremove(struct buf *b, int idx)
+{
+  struct buf *head;
+  head = bcache.buftable[idx];
+  if(!head){
+    return;
+  }
+  if(head == b){
+    bcache.buftable[idx] = b->next;
+    return;
+  }
+  while(head && head->next != b){
+    head = head->next;
+  }
+  if(head && head->next == b){
+    head->next = b->next;
+  }
+}
 
 void
 binit(void)
 {
   struct buf *b;
+  int i;
 
   initlock(&bcache.lock, "bcache");
+  
+  for(i = 0 ; i < BUF_TABLE_SIZE ; i++){
+    initlock(&bcache.btlock[i], "bcache");
+  }
 
-  // Create linked list of buffers
-  bcache.head.prev = &bcache.head;
-  bcache.head.next = &bcache.head;
-  for(b = bcache.buf; b < bcache.buf+NBUF; b++){
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
+  for(b = bcache.buf ; b < bcache.buf + NBUF ; b++)
+  {
+    btinsert(b, 0);
     initsleeplock(&b->lock, "buffer");
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
   }
 }
 
@@ -59,14 +101,18 @@ static struct buf*
 bget(uint dev, uint blockno)
 {
   struct buf *b;
+  int idx, i;
 
-  acquire(&bcache.lock);
+  idx = bthash(blockno);
+  
+  acquire(&bcache.btlock[idx]);
 
   // Is the block already cached?
-  for(b = bcache.head.next; b != &bcache.head; b = b->next){
+  for(b = bcache.buftable[idx]; b ; b = b->next){
     if(b->dev == dev && b->blockno == blockno){
       b->refcnt++;
-      release(&bcache.lock);
+      release(&bcache.btlock[idx]);
+      
       acquiresleep(&b->lock);
       return b;
     }
@@ -74,17 +120,35 @@ bget(uint dev, uint blockno)
 
   // Not cached.
   // Recycle the least recently used (LRU) unused buffer.
-  for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
-    if(b->refcnt == 0) {
-      b->dev = dev;
-      b->blockno = blockno;
-      b->valid = 0;
-      b->refcnt = 1;
-      release(&bcache.lock);
-      acquiresleep(&b->lock);
-      return b;
+  // * not lru for now
+  // * planning for a heap per bucket
+  // uint64 lru_min = -1;
+  for(i = 0 ; i < BUF_TABLE_SIZE ; i++){
+    if(i != idx){
+      acquire(&bcache.btlock[i]);
+    }
+    for(b = bcache.buftable[i] ; b ; b = b->next){
+      if(b->refcnt == 0) {
+        b->dev = dev;
+        b->blockno = blockno;
+        b->valid = 0;
+        b->refcnt = 1;
+        if(i != idx){
+          btremove(b, i);
+          release(&bcache.btlock[i]);
+          btinsert(b, idx);
+        }
+        release(&bcache.btlock[idx]);
+        
+        acquiresleep(&b->lock);
+        return b;
+      } 
+    }
+    if(i != idx){
+      release(&bcache.btlock[i]);
     }
   }
+
   panic("bget: no buffers");
 }
 
@@ -116,38 +180,45 @@ bwrite(struct buf *b)
 void
 brelse(struct buf *b)
 {
+  int idx;
   if(!holdingsleep(&b->lock))
     panic("brelse");
 
   releasesleep(&b->lock);
 
-  acquire(&bcache.lock);
+  idx = bthash(b->blockno);
+  
+  acquire(&bcache.btlock[idx]);
   b->refcnt--;
   if (b->refcnt == 0) {
     // no one is waiting for it.
-    b->next->prev = b->prev;
-    b->prev->next = b->next;
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+    b->timestamp = ticks;
   }
   
-  release(&bcache.lock);
+  release(&bcache.btlock[idx]);
+  
+}
+
+inline void
+_bpins(struct buf *b, int offset)
+{
+  int idx; 
+  idx = bthash(b->blockno);
+  
+  acquire(&bcache.btlock[idx]);
+  b->refcnt += offset;
+  release(&bcache.btlock[idx]);
+  
 }
 
 void
 bpin(struct buf *b) {
-  acquire(&bcache.lock);
-  b->refcnt++;
-  release(&bcache.lock);
+  _bpins(b, 1);
 }
 
 void
 bunpin(struct buf *b) {
-  acquire(&bcache.lock);
-  b->refcnt--;
-  release(&bcache.lock);
+  _bpins(b, -1);
 }
 
 
--- kernel/buf.h
+++ kernel/buf.h
@@ -5,8 +5,8 @@ struct buf {
   uint blockno;
   struct sleeplock lock;
   uint refcnt;
-  struct buf *prev; // LRU cache list
-  struct buf *next;
+  uint timestamp;   // LRU timestamp
+  struct buf *next; // hash table list
   uchar data[BSIZE];
 };
 
