2bf94c11c6484e1388a00cd83a9eaa982445d091
sora-blue <sora-blue@github.com>
2022-09-29 21:35:18 +0800

    lab-fs: pass symlinktest(?)

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -315,6 +315,22 @@ sys_open(void)
       return -1;
     }
   }
+  
+  if(ip->type == T_SYMLINK && !(omode & O_NOFOLLOW))
+  {
+    int rtimes = 0;
+    while(ip->type == T_SYMLINK)
+    {
+      readi(ip, 0, (uint64)path, 0, ip->size);
+      iunlockput(ip);
+      if(rtimes > 10 || (ip = namei(path)) == 0){
+        end_op();
+        return -1;
+      }
+      ilock(ip);
+      rtimes++;
+    }
+  }
 
   if(ip->type == T_DEVICE && (ip->major < 0 || ip->major >= NDEV)){
     iunlockput(ip);
@@ -482,5 +498,36 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64
+sys_symlink(void)
+{
+  int tlen;
+  struct inode *ip;
+  char target[MAXPATH], path[MAXPATH];
+
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0){
+    return -1;
+  }
+  tlen = strlen(target); 
+
+  begin_op();
+
+  // create a file which type is T_SYMLINK
+  ip = create(path, T_SYMLINK, 0, 0);
+  if(ip == 0){
+    end_op();
+    return -1;
+  }
+  // write target path string into inode
+  if(writei(ip, 0, (uint64)target, 0, tlen) != tlen)
+    panic("symlink: writei");
+
+  iupdate(ip);
+  iunlockput(ip);
+  end_op();
+  
   return 0;
 }
